586241a5e3be02d91bb002035a15b1afec361c8a
darren weeks <darren.weeks@iress.com>
2021-07-23 15:25:41 +0100

    change tag

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 docker build -f DockerFile-ssl -t creator:1.0 .
 
-docker build -f DockerFile-decrypt -t decryptor:1.0 .
+docker build -f DockerFile-decrypt -t akacodemonkey/decryptor:1.0 .
 
-docker build -f DockerFile-encrypt -t encryptor:1.0 .
+docker build -f DockerFile-encrypt -t akacodemonkey/encryptor:1.0 .
